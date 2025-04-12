745d8c1deac9b164fd358593603a85834ba70dd3
Zer0OnPaws <endove119@gmail.com>
2025-04-12 17:24:01 -0400

    Update uhacker.sh

--- uhacker.sh
+++ uhacker.sh
@@ -4,4 +4,5 @@ termux-wifi-enable false
 termux-torch on
 termux-brightness 255
 termux-vibrate -f -d 100
+termux-wallpaper -u https://static.wikia.nocookie.net/i-ready/images/8/8a/3dc8808484e9ae99ae1505432455828c.png/revision/latest\?cb\=20241228003207
 done
